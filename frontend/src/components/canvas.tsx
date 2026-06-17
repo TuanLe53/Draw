1bae770396427f05361cdc6a91c4a18c40afec39
TuanLe53 <leanhtuan030523@gmail.com>
2026-06-17 18:34:22 +0700

    Add Eraser

--- frontend/src/components/canvas.tsx
+++ frontend/src/components/canvas.tsx
@@ -1,11 +1,39 @@
-import { useRef } from "react";
+import { Eraser, Pencil } from "lucide-react";
+import { useRef, useState } from "react";
 import { ReactSketchCanvas, type ReactSketchCanvasRef } from "react-sketch-canvas";
 
 export default function Canvas(){
     const canvasRef = useRef<ReactSketchCanvasRef>(null);
+    const [eraseMode, setEraseMode] = useState(false);
+
+    const handleEraserClick = () =>{
+        setEraseMode(true);
+        canvasRef.current?.eraseMode(true);
+    }
+
+    const handlePenClick = () =>{
+        setEraseMode(false);
+        canvasRef.current?.eraseMode(false);
+    }
 
     return(
         <div>
+            <div>
+                <button
+                    type="button"
+                    onClick={handlePenClick}
+                >
+                    <Pencil />
+                    Pen
+                </button>
+                <button
+                    type="button"
+                    onClick={handleEraserClick}
+                >
+                    <Eraser />
+                    Eraser
+                </button>
+            </div>
             <ReactSketchCanvas
                 ref={canvasRef}
                 width="100%"
