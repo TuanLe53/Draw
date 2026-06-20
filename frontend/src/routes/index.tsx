5ac417e4339c6be0620ac15f4af361e1d00509a7
TuanLe53 <leanhtuan030523@gmail.com>
2026-06-20 14:34:22 +0700

    Create simple homepage

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,22 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { Presentation } from "lucide-react";
+
+export const Route = createFileRoute('/')({ component: Home })
+
+function Home() {
+    return (
+        <div className="p-8 flex justify-center items-center h-screen">
+            <div className="w-3/4 h-9/10 border-2 px-7 py-3 ">
+                <div className="flex flex-col justify-center items-center bg-blue-300">
+                    {/* Placeholder icon */}
+                    <Presentation size={128} />
+                    <p>THE TELEPHONE GAME</p>
+                </div>
+
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
