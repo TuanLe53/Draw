d73d8bdf8e870257814456c1341c0de5b4ef65a0
TuanLe53 <leanhtuan030523@gmail.com>
2026-06-22 12:14:47 +0700

    Add LoginSection component to home page and update layout

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import LoginSection from "#/components/home/LoginSection";
 import { createFileRoute } from "@tanstack/react-router";
 import { Presentation } from "lucide-react";
 
@@ -6,15 +7,16 @@ export const Route = createFileRoute('/')({ component: Home })
 function Home() {
     return (
         <div className="p-8 flex justify-center items-center h-screen">
-            <div className="w-3/4 h-9/10 border-2 px-7 py-3 ">
-                <div className="flex flex-col justify-center items-center bg-blue-300">
+            <div className="w-7/10 h-9/10 border-2 px-7 py-3 ">
+                <div className="flex flex-col justify-center items-center pb-4">
                     {/* Placeholder icon */}
                     <Presentation size={128} />
                     <p>THE TELEPHONE GAME</p>
                 </div>
 
-                <div>
-                    
+                <div className="flex flex-row gap-5">
+                    <LoginSection />
+                    <div className="grow-8 bg-blue-400"></div>
                 </div>
             </div>
         </div>
