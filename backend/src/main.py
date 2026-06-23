0eb15ba2047ec08cc80f27cb2b5f0e8b3988983d
TuanLe53 <leanhtuan030523@gmail.com>
2026-06-23 16:11:55 +0700

    Add await to lifespan func

--- backend/src/main.py
+++ backend/src/main.py
@@ -7,7 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 async def lifespan(app: FastAPI):
     app.state.redis = Redis(host="localhost", port=6379)
     yield
-    app.state.redis.close()
+    await app.state.redis.close()
 
 app = FastAPI(lifespan=lifespan)
 
